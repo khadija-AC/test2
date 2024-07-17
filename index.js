4349bc9e43d9a85c85fe05ca2b3624706fd2948f
admin <khadijaaws21@gmail.com>
2024-07-17 18:32:58 +0200

    feat(update): update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-(function (exports) {
+;(function (exports) {
   function greet(name) {
     const options = {
-      minute: "numeric",
-      hour: "numeric",
-      month: "long",
-      year: "numeric",
-      weekday: "numeric",
-    };
+      second: 'numeric',
+      minute: 'numeric',
+      hour: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'numeric',
+    }
 
-    const now = new Date();
-    const formattedDate = now.toLocaleDateString("en-US", options);
-    return `Hello, ${name}! Today is ${formattedDate}.`;
+    const now = new Date()
+    const formattedDate = now.toLocaleDateString('en-US', options)
+    return `Hello, ${name}! Today is ${formattedDate}.`
   }
 
-  exports.greet = greet;
-})(this);
+  exports.greet = greet
+})(this)
